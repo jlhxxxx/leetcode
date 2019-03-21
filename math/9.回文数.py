2082c7b443d337056c9428a2e0bf07e1673dba30
jlhxxxx <jlhxxxx@gmail.com>
2019-03-21 10:08:39 +0800

    push

--- math/9.回文数.py
+++ math/9.回文数.py
@@ -42,7 +42,7 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         y = str(x)
         z = y[::-1]
-        if y==z:
+        if y == z:
             return True
         else:
             return False
